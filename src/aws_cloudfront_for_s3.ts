66d03f89e7bf1e34fc9d3618d95d00db5b988522
Dorian <Plenituz@gmail.com>
2026-06-05 12:03:42 -0400

    wip: update cloudfront logging

--- src/aws_cloudfront_for_s3.ts
+++ src/aws_cloudfront_for_s3.ts
@@ -4,7 +4,7 @@ import { applyDefaults, compileBlockParam, preConfCloudResourceFactory, preConfT
 import {
     appendToTemplate,
     appendToTraversal,
-    asBlock, asFuncCall,
+    asBlock,
     asStr,
     asTemplate,
     asTraversal, asVal,
@@ -21,6 +21,42 @@ import md5 from "md5";
 const container = readDatabagContainer()
 onlyRunForLifecycleSteps(['pre_generate', 'generate', 'post_generate'])
 
+const cloudfrontLogColumns = [
+    ['date', 'string'],
+    ['time', 'string'],
+    ['x_edge_location', 'string'],
+    ['sc_bytes', 'string'],
+    ['c_ip', 'string'],
+    ['cs_method', 'string'],
+    ['cs_host', 'string'],
+    ['cs_uri_stem', 'string'],
+    ['sc_status', 'string'],
+    ['cs_referer', 'string'],
+    ['cs_user_agent', 'string'],
+    ['cs_uri_query', 'string'],
+    ['cs_cookie', 'string'],
+    ['x_edge_result_type', 'string'],
+    ['x_edge_request_id', 'string'],
+    ['x_host_header', 'string'],
+    ['cs_protocol', 'string'],
+    ['cs_bytes', 'string'],
+    ['time_taken', 'string'],
+    ['x_forwarded_for', 'string'],
+    ['ssl_protocol', 'string'],
+    ['ssl_cipher', 'string'],
+    ['x_edge_response_result_type', 'string'],
+    ['cs_protocol_version', 'string'],
+    ['fle_status', 'string'],
+    ['fle_encrypted_fields', 'string'],
+    ['c_port', 'string'],
+    ['time_to_first_byte', 'string'],
+    ['x_edge_detailed_result_type', 'string'],
+    ['sc_content_type', 'string'],
+    ['sc_content_len', 'string'],
+    ['sc_range_start', 'string'],
+    ['sc_range_end', 'string'],
+]
+
 
 function awsCfForS3Iterator(bag: Databag): (Databag | SugarCoatedDatabag)[] {
     if (!bag.Value) {
@@ -34,13 +70,14 @@ function awsCfForS3Iterator(bag: Databag): (Databag | SugarCoatedDatabag)[] {
     if(!block.s3_bucket) {
         throw new Error(`missing 's3_bucket' on aws_cloudfront_for_s3.${bag.Name}`)
     }
+    const s3Bucket = block.s3_bucket!
 
     const allBlockWithThisS3Bucket = Object.keys(container[AWS_CLOUDFRONT_FOR_S3]).filter(key => {
-        const [b] = applyDefaults(container, container[AWS_CLOUDFRONT_FOR_S3][key][0].Value)
-        return asStr(b.s3_bucket) === asStr(block.s3_bucket)
+        const [b] = applyDefaults(container, container[AWS_CLOUDFRONT_FOR_S3][key][0].Value!)
+        return asStr(b.s3_bucket!) === asStr(s3Bucket)
     })
     //hash might have a number in front so we put a _
-    const policyDocumentHash = '_' + md5(block.s3_bucket.Traversal[1].Name)
+    const policyDocumentHash = '_' + md5(s3Bucket.Traversal![1].Name!)
 
     const dotDomain = compileBlockParam(block, 'domain')
     const domainBlock = awsDomainBlockResources({
@@ -52,6 +89,7 @@ function awsCfForS3Iterator(bag: Databag): (Databag | SugarCoatedDatabag)[] {
         cloudResource,
     })
     const enabledLogs = block.enable_logging && asVal(block.enable_logging)
+    const enabledAthenaTable = enabledLogs && (block.enable_athena_table === undefined || asVal(block.enable_athena_table))
 
     let databags: SugarCoatedDatabag[] = [
         traversalTransform('aws_cf_for_s3_traversal_transform', {
@@ -72,7 +110,7 @@ function awsCfForS3Iterator(bag: Databag): (Databag | SugarCoatedDatabag)[] {
                 actions: ['s3:GetObject'],
                 resources: [
                     asTemplate([
-                        appendToTraversal(block.s3_bucket, 'arn'),
+                        appendToTraversal(s3Bucket, 'arn'),
                         '/*'
                     ])
                 ],
@@ -83,7 +121,7 @@ function awsCfForS3Iterator(bag: Databag): (Databag | SugarCoatedDatabag)[] {
             }])
         }),
         cloudResource('aws_s3_bucket_policy', `${policyDocumentHash}_cf_for_s3_policy`, {
-            bucket: appendToTraversal(block.s3_bucket, 'id'),
+            bucket: appendToTraversal(s3Bucket, 'id'),
             policy: asTraversal(`data.aws_iam_policy_document.${policyDocumentHash}_cf_for_s3_policy_document.json`)
         }),
         cloudResource('aws_cloudfront_distribution', `${bag.Name}_cf_for_s3`, {
@@ -98,7 +136,7 @@ function awsCfForS3Iterator(bag: Databag): (Databag | SugarCoatedDatabag)[] {
             }]),
 
             origin: asBlock([{
-                domain_name: appendToTraversal(block.s3_bucket, 'bucket_regional_domain_name'),
+                domain_name: appendToTraversal(s3Bucket, 'bucket_regional_domain_name'),
                 origin_id: 'bucket',
                 s3_origin_config: asBlock([{
                     origin_access_identity: asTraversal(`aws_cloudfront_origin_access_identity.${bag.Name}_cf_for_s3_oai.cloudfront_access_identity_path`)
@@ -134,16 +172,6 @@ function awsCfForS3Iterator(bag: Databag): (Databag | SugarCoatedDatabag)[] {
                     }
                 })()
             ]),
-            logging_config: enabledLogs ?
-                asBlock([{
-                    include_cookies: false,
-                    bucket: asTemplate([
-                        asTraversal(`aws_s3_bucket.${bag.Name}_cf_logs.bucket`),
-                        '.s3.amazonaws.com'
-                    ]),
-                    prefix: "logs",
-                }])
-                : null,
         })
     ]
     if(domainBlock) {
@@ -155,46 +183,102 @@ function awsCfForS3Iterator(bag: Databag): (Databag | SugarCoatedDatabag)[] {
                 bucket: appendToTemplate(namePrefix, [bag.Name, '-logs']),
                 force_destroy: block.force_destroy,
             }),
-            cloudResource('aws_s3_bucket_acl', `${bag.Name}_cf_logs_acl`, {
-                acl: "log-delivery-write",
-                bucket: asTraversal(`aws_s3_bucket.${bag.Name}_cf_logs.id`)
-            }),
-            cloudResource('aws_s3_bucket_ownership_controls', `${bag.Name}_cf_logs_ownership_controls`, {
-                bucket: asTraversal(`aws_s3_bucket.${bag.Name}_cf_logs.id`),
-                rule: asBlock([{
-                    object_ownership: "BucketOwnerPreferred"
-                }])
-            }),
             cloudResource('aws_s3_bucket_public_access_block', `${bag.Name}_cf_logs_access_block`, {
                 bucket: asTraversal(`aws_s3_bucket.${bag.Name}_cf_logs.id`),
-                block_public_acls: false,
-                block_public_policy: false,
-                ignore_public_acls: false,
-                restrict_public_buckets: false,
+                block_public_acls: true,
+                block_public_policy: true,
+                ignore_public_acls: true,
+                restrict_public_buckets: true,
             }),
-            cloudResource('aws_s3_bucket_policy', `${bag.Name}_cf_logs_access_block`, {
-                bucket: asTraversal(`aws_s3_bucket.${bag.Name}_cf_logs.id`),
-                policy: asFuncCall('jsonencode', [{
-                    Version: "2012-10-17",
-                    Statement: [{
-                        Action: "s3:PutObject",
-                        Effect: "Allow",
-                        Resource: asTemplate([
-                            asTraversal(`aws_s3_bucket.${bag.Name}_cf_logs.arn`),
-                            '/*'
-                        ]),
-                        Principal: {
-                            Service: "cloudfront.amazonaws.com",
-                        },
-                        Condition: {
-                            StringEquals: {
-                                "aws:SourceArn": asTraversal(`aws_cloudfront_distribution.${bag.Name}_cf_for_s3.arn`),
-                            }
-                        }
-                    }]
-                }])
+            cloudResource('aws_cloudwatch_log_delivery_source', `${bag.Name}_cf_logs_source`, {
+                region: 'us-east-1',
+                name: `${bag.Name}-cf-logs-source`,
+                log_type: 'ACCESS_LOGS',
+                resource_arn: asTraversal(`aws_cloudfront_distribution.${bag.Name}_cf_for_s3.arn`),
+            }),
+            cloudResource('aws_cloudwatch_log_delivery_destination', `${bag.Name}_cf_logs_destination`, {
+                region: 'us-east-1',
+                name: `${bag.Name}-cf-logs-s3-destination`,
+                output_format: 'parquet',
+                delivery_destination_configuration: asBlock([{
+                    destination_resource_arn: asTemplate([
+                        asTraversal(`aws_s3_bucket.${bag.Name}_cf_logs.arn`),
+                        '/logs',
+                    ]),
+                }]),
+            }),
+            cloudResource('aws_cloudwatch_log_delivery', `${bag.Name}_cf_logs_delivery`, {
+                region: 'us-east-1',
+                delivery_source_name: asTraversal(`aws_cloudwatch_log_delivery_source.${bag.Name}_cf_logs_source.name`),
+                delivery_destination_arn: asTraversal(`aws_cloudwatch_log_delivery_destination.${bag.Name}_cf_logs_destination.arn`),
+                s3_delivery_configuration: asBlock([{
+                    enable_hive_compatible_path: true,
+                    suffix_path: asTemplate([
+                        '{distributionid}/{yyyy}/{MM}/{dd}/{HH}',
+                    ]),
+                }]),
             }),
         )
+        if(enabledAthenaTable) {
+            const athenaDatabaseName = block.athena_database_name || `${bag.Name}_cf_logs`
+            const athenaTableName = block.athena_table_name || `${bag.Name}_cf_logs`
+            databags.push(
+                cloudResource('aws_glue_catalog_database', `${bag.Name}_cf_logs_database`, {
+                    name: athenaDatabaseName,
+                }),
+                cloudResource('aws_glue_catalog_table', `${bag.Name}_cf_logs_table`, {
+                    name: athenaTableName,
+                    database_name: asTraversal(`aws_glue_catalog_database.${bag.Name}_cf_logs_database.name`),
+                    table_type: 'EXTERNAL_TABLE',
+                    parameters: {
+                        EXTERNAL: 'TRUE',
+                        classification: 'parquet',
+                        'projection.enabled': 'true',
+                        'projection.distributionid.type': 'enum',
+                        'projection.distributionid.values': asTraversal(`aws_cloudfront_distribution.${bag.Name}_cf_for_s3.id`),
+                        'projection.year.type': 'integer',
+                        'projection.year.range': block.athena_projection_year_range || '2020,2030',
+                        'projection.month.type': 'integer',
+                        'projection.month.range': '01,12',
+                        'projection.month.digits': '2',
+                        'projection.day.type': 'integer',
+                        'projection.day.range': '01,31',
+                        'projection.day.digits': '2',
+                        'projection.hour.type': 'integer',
+                        'projection.hour.range': '00,23',
+                        'projection.hour.digits': '2',
+                        'storage.location.template': asTemplate([
+                            's3://',
+                            asTraversal(`aws_s3_bucket.${bag.Name}_cf_logs.bucket`),
+                            '/logs/distributionid=$${distributionid}/year=$${year}/month=$${month}/day=$${day}/hour=$${hour}/',
+                        ]),
+                    },
+                    partition_keys: asBlock([
+                        { name: 'distributionid', type: 'string' },
+                        { name: 'year', type: 'int' },
+                        { name: 'month', type: 'int' },
+                        { name: 'day', type: 'int' },
+                        { name: 'hour', type: 'int' },
+                    ]),
+                    storage_descriptor: asBlock([{
+                        location: asTemplate([
+                            's3://',
+                            asTraversal(`aws_s3_bucket.${bag.Name}_cf_logs.bucket`),
+                            '/logs/',
+                        ]),
+                        input_format: 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat',
+                        output_format: 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat',
+                        ser_de_info: asBlock([{
+                            serialization_library: 'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe',
+                            parameters: {
+                                'serialization.format': '1',
+                            },
+                        }]),
+                        columns: asBlock(cloudfrontLogColumns.map(([name, type]) => ({ name, type }))),
+                    }]),
+                }),
+            )
+        }
     }
     return databags
 }
